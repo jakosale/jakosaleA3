31bca37b519002662a15a2edb4ac9352c99b67b1
jakosale <52512105+jakosale@users.noreply.github.com>
2019-07-24 16:55:48 -0400

    Add files via upload

---
+++ assets/data/myInformation.ts
@@ -0,0 +1,13 @@
+import {Jakosale} from '../../app/jakosale'; 
+
+export const MYINFORMATION: Jakosale = 
+{
+        sid: 991000000, 
+        sname: 'Jeannelou Turbanos',
+        slogin: 'jakosale',
+        scampus: 'Sheridan College-Davis', 
+        sassignmenttitle:'Assignment3-Angular Intro'         
+}
+
+
+    
---
+++ assets/data/mySongs.ts
@@ -0,0 +1,31 @@
+import {SongInfo} from '../../app/songInfo'; 
+
+export const MYSONGS: SongInfo[] = [
+    {sngname:'Lose Yourself',
+     sngartist:'Eminem',
+     snggenre: 'Hip-hop',
+     yearreleased: '2002',
+     picture:'assets/images/loseYourself-Eminem.jpeg'
+},
+    {sngname:'The Nearness of You',
+    sngartist:'Nora Jones',
+    snggenre: 'Jazz',
+    yearreleased: '2002',
+    picture:'assets/images/theNearnessOfYou-NoraJones.jpg'
+},
+
+{sngname:'Thoughts About You',
+    sngartist:'Tim McGraw',
+    snggenre: 'Country',
+    yearreleased: '2019',
+    picture:'assets/images/thoughtAboutYou-TimMcGraw.jpg'
+},
+{sngname:'Crazy In Love',
+    sngartist:'Beyonce',
+    snggenre: 'R&B',
+    yearreleased: '2003',
+    picture:'assets/images/crazyInLove-Beyonce.jpeg'
+},
+
+];
+
